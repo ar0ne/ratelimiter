b53cad4b728f5977005d026a8215c673abaf7b96
ar0ne <camtbitakou@yandex.ru>
2024-01-05 16:20:06 +0530

    init project

---
+++ config/urls.py
@@ -0,0 +1,12 @@
+"""
+URL configuration for project.
+"""
+from django.contrib import admin
+from django.urls import path
+
+from api.views import api as api_v1
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/v1/", api_v1.urls),
+]
---
+++ ratelimits/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RatelimitsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ratelimits'
